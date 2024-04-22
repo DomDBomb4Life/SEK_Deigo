627100ec2d510959dc6e371e8c43b309bedbbac8
DomDBomb4Life <bachdominik06@icloud.com>
2024-04-22 18:06:47 -0500

    claw works now and not buggy (i hope) now the turning is buggy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PlaneLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PlaneLauncher.java
@@ -8,22 +8,40 @@ public class PlaneLauncher {
     Servo launcher;
     double open = 0.5;
     double closed = 0;
-    double position = closed;
+    boolean position = false;
     boolean isItLaunched = false;
     OpMode opMode;
     //constructor for launcher
     public PlaneLauncher(LinearOpMode opMode){
         launcher = opMode.hardwareMap.get(Servo.class,"LauncherServo");
         this.opMode = opMode;
+        launcher.setPosition(0);
     }
-    public void Launch(){
-        if(opMode.gamepad2.y && isItLaunched){
-            position = closed;
-            isItLaunched = false;
-        } else if (opMode.gamepad2.y && !isItLaunched) {
-            position = open;
+    public void Launch() {
+        //if(opMode.gamepad2.y && isItLaunched){
+        //position = closed;
+        //isItLaunched = false;
+        //} else if (opMode.gamepad2.y && !isItLaunched) {
+        //position = open;
+        //isItLaunched = true;
+
+        //launcher.setPosition(position);
+
+        if (opMode.gamepad2.y && !isItLaunched) {
+            position = !position; //toggle the state
             isItLaunched = true;
+            launcher.setPosition(0.5);
+        } else if (!opMode.gamepad2.y) {
+            isItLaunched = false; //button released, can be pressed again
+
         }
-        launcher.setPosition(position);
+        if (position) {
+            launcher.setPosition(0.5);
+            opMode.telemetry.addLine("Plane launched");
+        } else {
+            launcher.setPosition(0);
+            opMode.telemetry.addLine("Plane Docked");
+        }
     }
 }
+
